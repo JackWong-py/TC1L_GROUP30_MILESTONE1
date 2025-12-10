fffae5bfac7fad5bdc925d8bd067759619a23bf3
Jack Wong <jackuli1998@gmail.com>
2025-12-10 08:21:36 +0800

    wip: My local changes before pulling latest master.

--- main.cpp
+++ main.cpp
@@ -96,12 +96,3 @@ int main()
 
     return 0;
 }
-
-CHOICE show_menu()
-{
-    //Firstly, i need to cout the choice for user to choose
-    cout << //
-    //then get the choice fromsdukj abd
-
-
-}
